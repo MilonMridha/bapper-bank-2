a85d96658d20e4498c4587d4cacc747f40704213
MilonMridha <milonmridha570@gmail.com>
2022-02-15 23:34:56 -0800

    added total deposit

--- bank.js
+++ bank.js
@@ -13,8 +13,20 @@ const totalBalance = document.getElementById('balance-total');
 const previousTotalBalanceText = totalBalance.innerText;
 const previousTotalBalance = parseFloat(previousTotalBalanceText);
 totalBalance.innerText = previousTotalBalance + depositInputAmount;
-
-
-
-
+});
+//withdraw handle button
+document.getElementById('withdraw-button').addEventListener('click', function (){ const withdrawInput = document.getElementById('withdraw-input');
+const withdrawInputText = withdrawInput.value;
+const withdrawInputAmount = parseFloat(withdrawInputText);
+//withdraw Total update
+const withdrawTotal = document.getElementById('withdraw-total');
+const withdrawTotalText = withdrawTotal.innerText;
+const previousWithdrawAmount = parseFloat(withdrawTotalText);
+withdrawTotal.innerText = previousWithdrawAmount + withdrawInputAmount;
+// update Total withdrawBalance
+const totalBalance = document.getElementById('balance-total');
+const previousWithdrawBalanceText = totalBalance.innerText;
+const previousWithdrawTotalAmount = parseFloat(previousWithdrawBalanceText);
+totalBalance.innerText = previousWithdrawTotalAmount - withdrawInputAmount;
+withdrawInput.value = '';
 });
